06e5b514b2c963a2c7fdbad050920a3c847a9efc
Антон Кравцов <antonkravcov@msk-dpro-esx215.mp.x5.ru>
2023-06-23 11:24:01 +0300

    test commit

---
+++ heapsortAlgorithm/Main.java
@@ -0,0 +1,17 @@
+package heapsortAlgorithm;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array1 = new int[5];
+        array1[0] = 9;
+        array1[1] = 3;
+        array1[2] = 7;
+        array1[3] = 2;
+        array1[4] = 4;
+
+        HeapSort test = new HeapSort;
+        for (int i = 0; i < array1.length; i++) {
+            System.out.println(array1[i]);
+        }
+    }
+}
